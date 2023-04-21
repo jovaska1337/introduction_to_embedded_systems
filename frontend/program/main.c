de95db7794081efb8fdd3785d82d250c5f082e6f
jovaska <ovaska.juho@gmail.com>
2023-04-21 14:15:47 +0300

    frontend boot animation

--- frontend/program/main.c
+++ frontend/program/main.c
@@ -21,6 +21,99 @@ static stev_t stev_uistate = { .type = UI       };
 	stev_##what##state.now = to; \
 	dispatch(STATE, &stev_##what##state); })
 
+// timer ticks
+static u8 ticks;
+
+#define FRAME_TICKS 0
+#define TITLE_TICKS 20
+
+// cool boot animation, because why not
+static const chr boot_text_1[SCREEN_COLS] PROGMEM = "  ALARM SYSTEM  ";
+static const chr boot_text_2[SCREEN_COLS] PROGMEM = "    J.OVASKA    ";
+
+static u8 boot_counter;
+
+static u8 boot_sequence()
+{
+	// init
+	if (unlikely(boot_counter == 0)) {
+		screen_clear();
+		ev_set_id(PROGRAM_TIMER, 0);
+	}
+
+	// reveal
+	if (boot_counter < SCREEN_COLS) {
+		// line 1
+		if (boot_counter > 0) {
+			screen_goto(0, boot_counter - 1);
+			screen_putc(rom(boot_text_1[boot_counter - 1], byte), 0);
+		} else {
+			screen_goto(0, boot_counter);
+		}
+		screen_putc('#', 0);
+
+		// line 2
+		screen_goto(1, SCREEN_COLS - 1 - boot_counter);
+		screen_putc('#', 0);
+		if (boot_counter > 0)
+			screen_putc(rom(boot_text_2[SCREEN_COLS - boot_counter], byte), 0);
+
+		ticks = FRAME_TICKS;
+	
+	// keep text
+	} else if (boot_counter == SCREEN_COLS) {
+		// line 1
+		screen_goto(0, 15);
+		screen_putc(' ', 0);
+
+		// line 2
+		screen_goto(1, 0);
+		screen_putc(' ', 0);
+
+		ticks = TITLE_TICKS;
+	
+	// hide
+	} else if (boot_counter < 2*SCREEN_COLS) {
+		// line 1
+		screen_goto(0, 2*SCREEN_COLS - boot_counter - 1);
+		screen_putc('#', 0);
+		if (boot_counter > (SCREEN_COLS + 1))
+			screen_putc(' ', 0);
+
+		// line 2
+		if (boot_counter > (SCREEN_COLS + 1)) {
+			screen_goto(1, boot_counter - SCREEN_COLS - 1);
+			screen_putc(' ', 0);
+		} else {
+			screen_goto(1, boot_counter - SCREEN_COLS);
+		}
+		screen_putc('#', 0);
+
+		ticks = FRAME_TICKS;
+	
+	// end of animation
+	} else {
+		// line 1
+		screen_goto(0, 0);
+		screen_putc(' ', 0);
+
+		// line 2
+		screen_goto(1, 15);
+		screen_putc(' ', 0);
+
+		screen_flush();
+
+		boot_counter = 0;
+		return 1;
+	}
+
+	// commit to display
+	screen_flush();
+
+	boot_counter++;
+	return 0;
+}
+
 static void sstate_change(sstate_t old, sstate_t now)
 {
 	save_int();
@@ -58,6 +151,10 @@ static void istate_change(istate_t old, istate_t now)
 
 	switch (now) {
 	case BOOT: // booting up
+		// begin boot animation
+		(void) boot_sequence();
+
+		// TODO: attempt to discover link in background
 		break;
 
 	case LINK: // waiting for link
@@ -179,6 +276,37 @@ u8 e_button_input(u8 unused id, u8 unused code, u32 *arg)
 // timer event handler
 u8 e_program_timer(u8 unused id, u8 unused code, ptr unused arg)
 {
+	// wait
+	if (ticks-- > 0)
+		return 0;
+
+	// internal state overrides shared state
+	switch (istate) {
+	// boot state (turning on)
+	case BOOT:
+		// switch to on
+		if (boot_sequence()) {
+			// change state
+			change(i, LINK);
+
+			// disable self
+			ev_set_id(id, 1);
+		}
+		break;
+	
+	// waiting for link
+	case LINK:
+		break;
+
+	// waiting state (on)
+	case WAIT:
+		switch (sstate) {
+		// not configured
+		default:
+			ev_set_id(id, 1); // stop
+			break;
+		}
+	}
 	return 0;
 }
 
